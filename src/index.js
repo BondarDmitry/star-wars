d02ec5d4b6fe1970753e6c9b21ff0d82324c1224
bondardmitry <bondarconstr@gmail.com>
2020-07-21 21:53:33 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "./utils/axios-instance";
+
+class SwapiService {
+    getResource = async (url) => {
+        const response = await axios(url);
+        return response;
+    };
+
+    getAllPeople = async () => {
+        const res = await this.getResource(`/people/`);
+        return res.data.results;
+    };
+
+    getPerson = (id) => {
+        return this.getResource(`/people/${id}/`);
+    };
+}
+
+const service = new SwapiService();
+
+service.getAllPeople().then((res) => console.log(res));
+service.getPerson(2).then((res) => console.log(res));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div>Jello World</div>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
